ec28723101e3d88ff83fb9259f32b4850a6f4112
3bdalah <3bdallah.dev@gmail.com>
2023-08-13 01:51:35 +0300

    update ES6 / day-02

--- ES6/day01/app.js
+++ ES6/day01/app.js
@@ -1,113 +1,141 @@
-// assignment 01 
-(() => {
-    const firstNumber = 5;
-    const secondNumber = 10;
-    const sum = firstNumber + secondNumber;
-    alert(sum);
-})();
+// // assignment 01 
+// (() => {
+//     const firstNumber = 5;
+//     const secondNumber = 10;
+//     const sum = firstNumber + secondNumber;
+//     alert(sum);
+// })();
 
 
-// assignemnt 02 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // assignemnt 02 
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const oddNumbers = numbers.filter(number => number % 2 !== 0);
-console.log("Odd Numbers:", oddNumbers);
-
-
-
-numbers.forEach(number => {
-    if (number % 2 === 0) {
-        console.log("Even Number:", number);
-    }
-});
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+// console.log("Odd Numbers:", oddNumbers);
 
 
 
-const squares = numbers.map(number => number ** 2);
-console.log("Squared Numbers:", squares);
+// numbers.forEach(number => {
+//     if (number % 2 === 0) {
+//         console.log("Even Number:", number);
+//     }
+// });
 
 
 
-// Demos
-let elsayed = {
-     regularFunction  : function() {
-        console.log("Regular Func ----------> this:", this); // global  object
+// const squares = numbers.map(number => number ** 2);
+// console.log("Squared Numbers:", squares);
+
+
+
+// // Demos
+// let elsayed = {
+//      regularFunction  : function() {
+//         console.log("Regular Func ----------> this:", this); // global  object
         
-    },
-    name: "Mohesen",
-    childs : "mostafa",
-     arrowFunction : () => {
-        console.log("Arrow Func ------------> this:", this);
-    }
+//     },
+//     name: "Mohesen",
+//     childs : "mostafa",
+//      arrowFunction : () => {
+//         console.log("Arrow Func ------------> this:", this);
+//     }
+// };
+
+// console.log(elsayed.arrowFunction());
+// console.log(elsayed.regularFunction());
+
+
+
+
+// const colors = ['red', 'green', 'blue', 'yellow'];
+
+// console.log("Using for in :");
+// for (const index in colors) {
+//     console.log(index, colors[index]);
+// }
+
+// console.log("\nUsing for of :");
+// for (const color of colors) {
+//     console.log(color);
+// }
+
+
+
+// console.log("\nUsing .forEach() method:");
+// colors.forEach((color, index) => {
+//     console.log(index, color);
+// });
+
+
+
+// const tipsGenerator = function*() {
+//     const tips = [
+//         "Remember to back up your data regularly.",
+//         "Use strong and unique passwords for your accounts.",
+//         "Keep your software and operating systems up to date.",
+//         "Be cautious when clicking on links or downloading attachments.",
+//         "Enable two-factor authentication whenever possible.",
+//         "Be mindful of sharing personal information online.",
+//         "Regularly review your privacy settings on social media.",
+//         "Use a reliable antivirus and anti-malware software.",
+//         "Avoid using public Wi-Fi for sensitive activities.",
+//         "Don't ignore security warnings from websites or applications."
+//     ];
+
+//     for (const tip of tips) {
+//         yield tip;
+//     }
+// };
+
+// const displayBtn = document.getElementById("displayButton");
+// const intervalButton = document.getElementById('intervalButton');
+// const tipDisplay = document.getElementById('tipDisplay');
+
+// displayBtn.addEventListener('click', () => {
+//     tipDisplay.innerHTML = "";
+//     const tips = tipsGenerator();
+//     for (const tip of tips) {
+//         tipDisplay.innerHTML += `${tip}<br>`;
+//     }
+// });
+
+// intervalButton.addEventListener('click', () => {
+//     tipDisplay.innerHTML = "";
+//     const tips = tipsGenerator();
+//     const interval = setInterval(() => {
+//         const nextTip = tips.next();
+//         if (nextTip.done) {
+//             clearInterval(interval);
+//         } else {
+//             tipDisplay.innerHTML += `${nextTip.value}<br>`;
+//         }
+//     }, 3000);
+// });
+
+
+
+
+
+// New Featured ES6 
+// array destructure 
+const numbersss = [1, 2, 3];
+const [first, second, third] = numbersss;
+// object Desrturcture 
+const person = { name: "Bob", age: 30 };
+const { name, age } = person;
+
+
+
+
+
+
+
+// 
+const propKey = 'name';
+const personObject = {
+  [propKey]: 'Alice',
+  age: 25
 };
 
-console.log(elsayed.arrowFunction());
-console.log(elsayed.regularFunction());
 
-
-
-
-const colors = ['red', 'green', 'blue', 'yellow'];
-
-console.log("Using for in :");
-for (const index in colors) {
-    console.log(index, colors[index]);
-}
-
-console.log("\nUsing for of :");
-for (const color of colors) {
-    console.log(color);
-}
-
-
-
-console.log("\nUsing .forEach() method:");
-colors.forEach((color, index) => {
-    console.log(index, color);
-});
-
-
-
-const tipsGenerator = function*() {
-    const tips = [
-        "Remember to back up your data regularly.",
-        "Use strong and unique passwords for your accounts.",
-        "Keep your software and operating systems up to date.",
-        "Be cautious when clicking on links or downloading attachments.",
-        "Enable two-factor authentication whenever possible.",
-        "Be mindful of sharing personal information online.",
-        "Regularly review your privacy settings on social media.",
-        "Use a reliable antivirus and anti-malware software.",
-        "Avoid using public Wi-Fi for sensitive activities.",
-        "Don't ignore security warnings from websites or applications."
-    ];
-
-    for (const tip of tips) {
-        yield tip;
-    }
-};
-
-const displayBtn = document.getElementById("displayButton");
-const intervalButton = document.getElementById('intervalButton');
-const tipDisplay = document.getElementById('tipDisplay');
-
-displayBtn.addEventListener('click', () => {
-    tipDisplay.innerHTML = "";
-    const tips = tipsGenerator();
-    for (const tip of tips) {
-        tipDisplay.innerHTML += `${tip}<br>`;
-    }
-});
-
-intervalButton.addEventListener('click', () => {
-    tipDisplay.innerHTML = "";
-    const tips = tipsGenerator();
-    const interval = setInterval(() => {
-        const nextTip = tips.next();
-        if (nextTip.done) {
-            clearInterval(interval);
-        } else {
-            tipDisplay.innerHTML += `${nextTip.value}<br>`;
-        }
-    }, 3000);
-});
+console.log("peson object : ", personObject);
--- ES6/day02/app.js
+++ ES6/day02/app.js
@@ -1,37 +1,101 @@
-const userDropdown = document.getElementById('userDropdown');
-    const showDetailsButton = document.getElementById('showDetailsButton');
-    const userDetailsDiv = document.getElementById('userDetails');
-    let users = [];
+// assignemnt 01
+const userDropdown = document.getElementById("userDropdown");
+const showDetailsButton = document.getElementById("showDetailsButton");
+const userDetailsDiv = document.getElementById("userDetails");
+let users = [];
 
-    // Fetch user data and populate dropdown
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        users = data;
-        users.forEach(user => {
-          const option = document.createElement('option');
-          option.value = user.id;
-          option.textContent = user.name;
-          userDropdown.appendChild(option);
-        });
-        showDetailsButton.disabled = false;
-      })
-      .catch(error => console.error('Error fetching users:', error));
-
-    // Show user details on button click
-    showDetailsButton.addEventListener('click', () => {
-      const selectedUserId = userDropdown.value;
-      const selectedUser = users.find(user => user.id === parseInt(selectedUserId));
-
-      if (selectedUser) {
-        userDetailsDiv.innerHTML = `
-          <h2>User Details:</h2>
-          <p>Name: ${selectedUser.name}</p>
-          <p>Email: ${selectedUser.email}</p>
-          <p>Phone: ${selectedUser.phone}</p>
-          <!-- Add more user details as needed -->
-        `;
-      } else {
-        userDetailsDiv.innerHTML = '';
-      }
+async function fetchUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    users = data;
+    users.forEach((user) => {
+      const option = document.createElement("option");
+      option.value = user.id;
+      option.textContent = user.name;
+      userDropdown.appendChild(option);
     });
+    showDetailsButton.disabled = false;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
+
+fetchUsers();
+
+showDetailsButton.addEventListener("click", () => {
+  const selectedUserId = userDropdown.value;
+  const selectedUser = users.find(
+    (user) => user.id === parseInt(selectedUserId)
+  );
+
+  if (selectedUser) {
+    userDetailsDiv.innerHTML = `
+      <h2>User Details:</h2>
+      <p>Name: ${selectedUser.name}</p>
+      <p>Email: ${selectedUser.email}</p>
+      <p>Phone: ${selectedUser.phone}</p>
+      <!-- Add more user details as needed -->
+    `;
+  } else {
+    userDetailsDiv.innerHTML = "";
+  }
+});
+
+// assignemnt 03
+const newPost = {
+  title: "foo",
+  body: "bar",
+  userId: 1,
+};
+
+async function addNewPost() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    });
+
+    const data = await response.json();
+    console.log("new post added:", data);
+  } catch (error) {
+    console.error("error adding new post:", error);
+  }
+}
+
+addNewPost();
+
+// assignment 04
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.onerror = () => reject(new Error("Failed to load image"));
+    image.src = url;
+  });
+}
+
+function showMessage(message, delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(message);
+      resolve();
+    }, delay);
+  });
+}
+
+showMessage("Welcome!", 3000)
+  .then(() => loadImage("./assests/twitter.png"))
+  .then((image) => {
+    document.body.appendChild(image);
+    return showMessage("Thanks!", 3000);
+  })
+  .then(() => {
+    window.location.href = "another.html";
+  })
+  .catch((error) => {
+    console.error(error);
+  });
