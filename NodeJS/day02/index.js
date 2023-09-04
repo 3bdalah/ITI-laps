669981fa95dc6403319635b0d0aa6634a40a7c5f
3bdalah <3bdallah.dev@gmail.com>
2023-09-04 17:52:55 +0300

    update: NodeJS [Day 02]

--- NodeJS/day02/index.js
+++ NodeJS/day02/index.js
@@ -126,3 +126,139 @@ const newServ = abdo.createServer(function (req, res) {
   }
 });
 newServ.listen(4000);
+
+//  ===============================================================================================================
+// Express Setup:
+const express = require("express");
+const app = express();
+const port = 4000;
+
+let users = [
+  { id: 1, name: "Abdallah", age: 12 },
+  { id: 3, name: "zaki", age: 22 },
+  { id: 2, name: "nour", age: 32 },
+];
+
+let posts = [
+  { id: 1, content: "tooltest test bla bla bla bfkmf", likes: 2 },
+  { id: 2, content: "fvfvknfvnfv vjfnjvnsdjv", likes: 12 },
+];
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HOME");
+});
+
+// START USERS
+app.get("/getallusers", (req, res) => {
+  res.json(users);
+});
+
+app.get("/getuserssorted", (req, res) => {
+  const sortedUsers = users.sort((a, b) => a.name.localeCompare(b.name));
+  users = sortedUsers;
+  res.json(users);
+});
+
+app.post("/adduser", (req, res) => {
+  const newUser = req.body;
+  posts.push(newUser);
+  res.json(users);
+});
+
+app.delete("/deleteuser", (req, res) => {
+  const idToDelete = req.body.id;
+  users = users.filter((user) => user.id !== idToDelete);
+  res.json(users);
+});
+
+app.put("/updateuser", (req, res) => {
+  const updatedUser = req.body;
+  users = users.map((user) => {
+    if (user.id === updatedPost.id) {
+      return {
+        ...user,
+        age: updatedUser.age,
+        name: updatedUser.name,
+      };
+    }
+    return user;
+  });
+  res.json(users);
+});
+// wait aminute just try another method ok :)
+app.get("/getusertbyid", (req, res) => {
+  const userId = req.query.id;
+  const user = posts.find((u) => u.id === parseInt(userId));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("NOT FOUND");
+  }
+});
+//  another method to get user by id  ok
+app.get("/:iduser", (req, res) => {
+  let { iduser } = req.params;
+  const user = posts.find((user) => user.id == iduser);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("NOT FOUND");
+  }
+});
+// START POSTS
+app.get("/getallposts", (req, res) => {
+  res.json(posts);
+});
+
+app.get("/getpostssorted", (req, res) => {
+  const sortedPosts = posts.sort((a, b) => a.name.localeCompare(b.name));
+  posts = sortedPosts;
+  res.json(posts);
+});
+
+app.post("/addpost", (req, res) => {
+  const newPost = req.body;
+  posts.push(newPost);
+  res.json(posts);
+});
+
+app.delete("/deletepost", (req, res) => {
+  const idToDelete = req.body.id;
+  posts = posts.filter((post) => post.id !== idToDelete);
+  res.json(posts);
+});
+
+app.put("/updatepost", (req, res) => {
+  const updatedPost = req.body;
+  posts = posts.map((post) => {
+    if (post.id === updatedPost.id) {
+      return {
+        ...post,
+        age: updatedPost.age,
+        name: updatedPost.name,
+      };
+    }
+    return post;
+  });
+  res.json(posts);
+});
+
+app.get("/getpostbyid", (req, res) => {
+  const userId = req.query.id;
+  const post = posts.find((u) => u.id === parseInt(userId));
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send("NOT FOUND");
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send("NOT FOUND");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
