528655a028e68f0834f2a54eb31c0e1a89447049
3bdalah <3bdallah.dev@gmail.com>
2023-09-17 21:50:44 +0300

    delete: [Day-05] at NodeJs

--- NodeJS/day05/modules/users/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import signUp from "./controller/singup.js";
-import handleValidation from "../../middleware/handleValidation.js";
-import signUpValidator from "./validation.js";
-const userRouter = Router();
-
-userRouter.post("/signup", signUpValidator, handleValidation, signUp);
-
-export default userRouter;
--- NodeJS/day05/modules/users/validation.js
+++
@@ -1,8 +0,0 @@
-import { body, validationResult } from "express-validator";
-const signUpValidator = [
-  body("username").matches(/^[a-zA-Z][a-zA-Z0-9_-]{3,15}$/),
-  body("email").isEmail(),
-  body("password").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/),
-];
-
-export default signUpValidator;
