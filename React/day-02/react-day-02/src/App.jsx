520a46539fcec08aacf53cfa7e4830edb7308191
3bdalah <3bdallah.dev@gmail.com>
2023-10-08 03:25:15 +0300

    feat(reactJS): add new task day-02 at react

---
+++ React/day-02/react-day-02/src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+export default function App() {
+  return (
+    <div>
+      App
+      <h6 className="text-3xl text-center  font-bold underline text-yellow-400">
+        Hello world!
+      </h6>
+      <button className="btn text-3xl bg-stone-200  rounded w-96 h-16">
+        increase counter
+      </button>
+    </div>
+  );
+}
